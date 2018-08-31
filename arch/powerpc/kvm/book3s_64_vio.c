0293c694713b661da38932f3468529199f101637
Alexey Kardashevskiy <aik@ozlabs.ru>
2018-08-31 15:55:42 +1000

    KVM: PPC: Remove redundand permission bits removal
    https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1800649
    
    The kvmppc_gpa_to_ua() helper itself takes care of the permission
    bits in the TCE and yet every single caller removes them.
    
    This changes semantics of kvmppc_gpa_to_ua() so it takes TCEs
    (which are GPAs + TCE permission bits) to make the callers simpler.
    
    This should cause no behavioural change.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    ---
    Changes:
    v2:
    * %s/kvmppc_gpa_to_ua/kvmppc_tce_to_ua/g
    (cherry picked from commit 778f1b80a9f6a69c2dad874c9c520d2fced61761)
    Signed-off-by: Jose Ricardo Ziviani <joserz@linux.ibm.com>

--- arch/powerpc/kvm/book3s_64_vio.c
+++ arch/powerpc/kvm/book3s_64_vio.c
@@ -381,8 +381,7 @@ static long kvmppc_tce_validate(struct kvmppc_spapr_tce_table *stt,
 	if (iommu_tce_check_gpa(stt->page_shift, gpa))
 		return H_TOO_HARD;
 
-	if (kvmppc_gpa_to_ua(stt->kvm, tce & ~(TCE_PCI_READ | TCE_PCI_WRITE),
-				&ua, NULL))
+	if (kvmppc_tce_to_ua(stt->kvm, tce, &ua, NULL))
 		return H_TOO_HARD;
 
 	list_for_each_entry_rcu(stit, &stt->iommu_tables, next) {
