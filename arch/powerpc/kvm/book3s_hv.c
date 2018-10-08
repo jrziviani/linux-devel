ac6679b59ef34035eb23b481ff3e6558cb5f2f48
Paul Mackerras <paulus@ozlabs.org>
2018-10-08 16:31:13 +1100

    KVM: PPC: Book3S HV: Add one-reg interface to virtual PTCR register
    BugLink: https://canonical.my.salesforce.com/5003z00001xnYEC (KVM: Back port POWER9 nested virtualization patches (SF: #00228446))
    
    This adds a one-reg register identifier which can be used to read and
    set the virtual PTCR for the guest.  This register identifies the
    address and size of the virtual partition table for the guest, which
    contains information about the nested guests under this guest.
    
    Migrating this value is the only extra requirement for migrating a
    guest which has nested guests (assuming of course that the destination
    host supports nested virtualization in the kvm-hv module).
    
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (backported from commit 3032341853daf07c723084a086ebe7a9bdf8f90b)
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Signed-off-by: Leonardo Garcia <lagarcia@br.ibm.com>
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- arch/powerpc/kvm/book3s_hv.c
+++ arch/powerpc/kvm/book3s_hv.c
@@ -1677,6 +1677,9 @@ static int kvmppc_get_one_reg_hv(struct kvm_vcpu *vcpu, u64 id,
 		*val = get_reg_val(id, vcpu->arch.dec_expires +
 				   vcpu->arch.vcore->tb_offset);
 		break;
+	case KVM_REG_PPC_PTCR:
+		*val = get_reg_val(id, vcpu->kvm->arch.l1_ptcr);
+		break;
 	default:
 		r = -EINVAL;
 		break;
@@ -1899,6 +1902,9 @@ static int kvmppc_set_one_reg_hv(struct kvm_vcpu *vcpu, u64 id,
 	case KVM_REG_PPC_DEC_EXPIRY:
 		vcpu->arch.dec_expires = set_reg_val(id, *val) -
 			vcpu->arch.vcore->tb_offset;
+		break;
+	case KVM_REG_PPC_PTCR:
+		vcpu->kvm->arch.l1_ptcr = set_reg_val(id, *val);
 		break;
 	default:
 		r = -EINVAL;
