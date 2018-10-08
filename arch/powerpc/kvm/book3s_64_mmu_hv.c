66d948902b5f89cd21e8dcbc2caf13519d2938ce
Paul Mackerras <paulus@ozlabs.org>
2018-10-08 16:31:12 +1100

    KVM: PPC: Book3S HV: Don't access HFSCR, LPIDR or LPCR when running nested
    BugLink: https://canonical.my.salesforce.com/5003z00001xnYEC (KVM: Back port POWER9 nested virtualization patches (SF: #00228446))
    
    When running as a nested hypervisor, this avoids reading hypervisor
    privileged registers (specifically HFSCR, LPIDR and LPCR) at startup;
    instead reasonable default values are used.  This also avoids writing
    LPIDR in the single-vcpu entry/exit path.
    
    Also, this removes the check for CPU_FTR_HVMODE in kvmppc_mmu_hv_init()
    since its only caller already checks this.
    
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    (backported from commit f3c99f97a3cda284418af6e242e06b351b83f647)
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Signed-off-by: Leonardo Garcia <lagarcia@br.ibm.com>
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- arch/powerpc/kvm/book3s_64_mmu_hv.c
+++ arch/powerpc/kvm/book3s_64_mmu_hv.c
@@ -269,11 +269,10 @@ int kvmppc_mmu_hv_init(void)
 {
 	unsigned long host_lpid, rsvd_lpid;
 
-	if (!cpu_has_feature(CPU_FTR_HVMODE))
-		return -EINVAL;
-
 	/* POWER7 has 10-bit LPIDs (12-bit in POWER8) */
-	host_lpid = mfspr(SPRN_LPID);
+	host_lpid = 0;
+	if (cpu_has_feature(CPU_FTR_HVMODE))
+		host_lpid = mfspr(SPRN_LPID);
 	rsvd_lpid = LPID_RSVD;
 
 	kvmppc_init_lpid(rsvd_lpid + 1);
