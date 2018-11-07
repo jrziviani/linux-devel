51751822ee371fa3d3c9dc94122d008e31eef8db
Raed Salem <raeds@mellanox.com>
2018-11-07 15:44:37 +0200

    IB/mlx5: E-Switch, Add rule to forward traffic to vport
    In order to forward traffic from PF's SQ to the right virtual
    function, every time an SQ is created also add the corresponding flow rule
    to the FDB, if we operate in switchdev mode
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    
    [juergh: IBM patchset mlx-4.15.0-24.26-patches-20181119 (SF #00205874).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -2087,3 +2087,9 @@ free_out:
 	kvfree(out);
 	return err;
 }
+
+u8 mlx5_eswitch_mode(struct mlx5_eswitch *esw)
+{
+	return ESW_ALLOWED(esw) ? esw->mode : SRIOV_NONE;
+}
+EXPORT_SYMBOL_GPL(mlx5_eswitch_mode);
