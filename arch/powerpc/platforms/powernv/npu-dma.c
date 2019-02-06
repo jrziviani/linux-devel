10c28aa4204f57d5b21a80263809cbfe8f1f930a
Jose Ricardo Ziviani <joserz@linux.ibm.com>
2019-02-06 21:14:26 -0200

    Revert "powerpc/powernv/npu: Simplify nestMMU flush flag copying"
    BugLink: https://bugs.launchpad.net/bugs/1800649
    
    This reverts commit 6e3becda056b9405019046db8198c03418b2133b.
    
    Signed-off-by: Jose Ricardo Ziviani <joserz@linux.ibm.com>
    [juergh: IBM patchset nvlink2-based-on-ibmgt.tar.gz (SF #00213341).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- arch/powerpc/platforms/powernv/npu-dma.c
+++ arch/powerpc/platforms/powernv/npu-dma.c
@@ -824,13 +824,15 @@ struct npu_context *pnv_npu2_init_context(struct pci_dev *gpdev,
 	 */
 	WRITE_ONCE(npu_context->npdev[npu->index][nvlink_index], npdev);
 
-	npu_context->nmmu_flush = npu->nmmu_flush;
-	if (!npu->nmmu_flush)
+	if (!nphb->npu.nmmu_flush) {
 		/*
 		 * If we're not explicitly flushing ourselves we need to mark
 		 * the thread for global flushes
 		 */
+		npu_context->nmmu_flush = false;
 		mm_context_add_copro(mm);
+	} else
+		npu_context->nmmu_flush = true;
 
 	return npu_context;
 }
